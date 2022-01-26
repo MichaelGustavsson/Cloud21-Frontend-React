956142262c8407ae8340cbc2262d5a22519e9f64
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-01-26 07:51:06 +0100

    Added navbar component and basic css

---
+++ dag-3/final/Readme.md
@@ -0,0 +1,3 @@
+# Start projekt för Westcoast Cars applikationen
+
+Detta är ett nytt bas projekt för Westcoast Cars
---
+++ dag-3/final/westcoast-cars/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from './components/navbar/Navbar';
+
+import './styles.css';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
